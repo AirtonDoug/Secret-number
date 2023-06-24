03a6583d18cc56a48a5b12588dfb7b63edb972ee
AirtonDoug <airtonservodecristo@gmail.com>
2023-06-24 11:37:18 -0300

    secretnumber app

--- app/speechRecognition.js
+++ app/speechRecognition.js
@@ -2,23 +2,34 @@ const guessElement = document.getElementById('chute');
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
-
 recognition.lang = 'en-US';
-recognition.start();
 
-recognition.addEventListener('result', onSpeak)
+// Request microphone permission when the page loads
+document.addEventListener('DOMContentLoaded', () => {
+  askForMicrophonePermission();
+});
 
-function onSpeak(e){
-    guess = e.results[0][0].transcript;
-    showGuess(guess);
-    checkGuess(guess);
+function askForMicrophonePermission() {
+  recognition.start();
+  recognition.stop();
 }
 
-function showGuess(guess){
-    guessElement.innerHTML=`
-    <div>Você disse </div>
-    <span class = "box">${guess}</span>
-    `
+recognition.addEventListener('result', onSpeak);
+
+function onSpeak(e) {
+  const guess = e.results[0][0].transcript;
+  showGuess(guess);
+  checkGuess(guess);
 }
 
-recognition.addEventListener('end',()=> recognition.start())
+function showGuess(guess) {
+  guessElement.innerHTML = `
+    <div>Você disse:</div>
+    <span class="box">${guess}</span>
+  `;
+}
+
+recognition.addEventListener('end', () => {
+  // Restart speech recognition after it ends
+  recognition.start();
+});
